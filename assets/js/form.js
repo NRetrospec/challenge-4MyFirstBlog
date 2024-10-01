9b897abcfaa5250852ea0ce20b001d7c49545eaf
Phresh . <marquisej.galo@gmail.com>
2024-10-01 17:29:08 -0400

    add event listner to form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,15 +1,25 @@
 const formBlock = document.querySelector("form");
 
-const formSubmit = () => {
-  const usernameEl = document.getElementById("#username").value;
-  const titleEl = document.getElementById("#title").value;
-  const contentEl = document.getElementById("#content").value;
-  
-  const blogData = { 
-    name:usernameEl,
-    title:titleEl,
+const formSubmit = (event) => {
+  event.preventDefault();
+  const usernameEl = document.getElementById("username").value;
+  const titleEl = document.getElementById("title").value;
+  const contentEl = document.getElementById("content").value;
+
+  if (!usernameEl || !titleEl || !contentEl) {
+    alert("Please complete all fields");
+  }
+
+
+
+
+
+  const blogData = {
+    name: usernameEl,
+    title: titleEl,
     content: contentEl,
   };
+  console.log(blogData)
+};
 
-} 
-
+formBlock.addEventListener("submit", formSubmit);
