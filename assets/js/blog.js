57a7cc88a90a837192c07bca3ec24bcfa78291fb
Phresh . <marquisej.galo@gmail.com>
2024-10-03 16:49:06 -0400

    adding general function to create and append text elements

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,1 +1,30 @@
 const mainBlock = document.querySelector("main")
+
+
+
+// const blogTitle = document.createElement("h2");
+// blogTitle.textContent = blogData.title;
+// mainBlock.appendChild(blogTitle);
+
+// const blogContent = document.createElement("p");
+// blogContent.textContent = blogData.content;
+// mainBlock.appendChild(blogContent);
+
+// const blogData = {
+//   name: usernameEl,
+//   title: titleEl,
+//   content: contentEl,
+// };
+const tagMaker = function(elementType, text, parent){
+  const tags = document.createElement(elementType);
+  tags.textContent = text;
+  parent.appendChild(tags);
+
+};
+
+tagMaker("h2", blogData.title, mainBlock);
+tagMaker("p", blogData.content, mainBlock);
+tagMaker("p",blogData.name, mainBlock);
+
+
+
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,7 +1,9 @@
 const formBlock = document.querySelector("form");
 
+// Function to handle when user hits submit button
 const formSubmit = (event) => {
   event.preventDefault();
+  console.log(event)
   const usernameEl = document.getElementById("username").value;
   const titleEl = document.getElementById("title").value;
   const contentEl = document.getElementById("content").value;
